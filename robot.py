ea7fb8361b68362d73a7b263575e4d869c632b11
Blue-Kod <logaa2401@gmail.com>
2024-05-22 16:55:01 +0300

    Update robot.py

--- robot.py
+++ robot.py
@@ -5,7 +5,6 @@ import threading
 import cv2
 import numpy as np
 import speech_recognition as sr
-from speakerpy.lib_speak import Speaker
 
 
 def short():
@@ -13,7 +12,7 @@ def short():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # получаем имя хоста сервера
-    host = socket.gethostname()
+    host = "192.168.0.187"
     port = 10000
 
     # связываем сокет с хостом и портом
@@ -21,6 +20,7 @@ def short():
 
     # слушаем подключения
     server_socket.listen(1)
+    print("Short started")
 
     def handle_client(client_socket):
         cap = cv2.VideoCapture(0)
@@ -53,7 +53,7 @@ def long():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # получаем имя хоста сервера
-    host = socket.gethostname()
+    host = "192.168.0.187"
     port = 10001
 
     # связываем сокет с хостом и портом
@@ -61,6 +61,7 @@ def long():
 
     # слушаем подключения
     server_socket.listen(1)
+    print("Long started")
 
     def handle_client(client_socket):
         r = sr.Recognizer()
@@ -83,10 +84,8 @@ def long():
                     except:
                         client_socket.send("".encode())
                 elif command.startswith("voice"):
-                    tts = Speaker(model_id="ru_v3", language="ru", speaker=command.replace("voice ", "").lower(), device="cpu")
                     client_socket.send("OK".encode())
                 elif command.startswith("say"):
-                    tts.speak(command.replace("say ", ""), sample_rate=24000, speed=1)
                     client_socket.send("OK".encode())
     while True:
         client_socket, addr = server_socket.accept()
